dc48b49d2751bebf85b4d58d5b40cbc0133a1d3d
Bram <kolkmanbram@hotmail.com>
2020-11-15 01:39:02 +0100

    Random implementation
    * Implemented the random data generation

--- Sources/CCSwift/Utils.swift
+++ Sources/CCSwift/Utils.swift
@@ -1,6 +1,21 @@
+import CCWrapper
 import Foundation
 
 public extension Data {
+    /// Generate secure random data.
+    /// - Parameter size: The number of bytes to generate.
+    /// - Returns: The generated random data.
+    static func random(ofSize size: Int) -> Data {
+        let ccRandomBytes = try! CCRandomGenerateBytes(count: size)         // CommonCrypto random
+        
+        var secRandomBytes = Data(repeating: 0, count: size)                // Security random
+        _ = secRandomBytes.withUnsafeMutableBytes {
+            SecRandomCopyBytes(kSecRandomDefault, size, $0.baseAddress!)
+        }
+        
+        return ccRandomBytes ^ secRandomBytes                               // XOR to get the best entropy
+    }
+    
     /// Perform a constant time safe compare.
     /// - Parameters:
     ///   - lhs: The left hand side of the operation.
@@ -11,5 +26,15 @@ public extension Data {
             return false
         }
         return zip(lhs, rhs).reduce(into: 0) { $0 |= $1.0 ^ $1.1 } == 0
+    }
+    
+    /// Perform the XOR operation on two `Data` objects.
+    /// - Parameters:
+    ///   - lhs: The left hand side of the operation.
+    ///   - rhs: The right hand side of the operation.
+    /// - Returns: The xor'ed data.
+    internal static func ^ (_ lhs: Data, _ rhs: Data) -> Data {
+        precondition(lhs.count == rhs.count, "Cannot XOR data of unequal size.")
+        return Data(zip(lhs, rhs).map { $0 ^ $1 })
     }
 }
